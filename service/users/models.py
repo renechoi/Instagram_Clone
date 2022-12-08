c9410fb7b88c8636ea69a3759c72eddffc4a3e19
renechoi <renechoi90@gmail.com>
2022-12-09 06:23:29 +0900

    implement password authentication

--- service/users/models.py
+++ service/users/models.py
@@ -1,8 +1,12 @@
+# python modules
+import time
+import hashlib
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.base_user import BaseUserManager
+from django.core.exceptions import ValidationError
 
-# Create your models here.
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password, **extra_fields):
@@ -37,5 +41,75 @@ class User(AbstractUser):
     objects = UserManager()
     user_manager = UserManager()
     
+    def __repr__(self):
+        return f"<User {self.pk} {self.username} {self.updated}>"
+    
     class Meta:
         ordering =['created']
+        
+        
+    def _create_authcode(self):
+        timestamp = int(time.time())
+        while True:
+            authcode = hashlib.sha224(f"{repr(self)}:{timestamp}".encode()).hexdigest()[:6]
+            try:
+                User.objects.get(authcode=authcode)
+            except User.DoesNotExist:
+                self.authcode = f"{authcode}:{timestamp}"
+                break
+        self.save()
+        return authcode
+
+    def create_authcode(self):
+        if self.authcode != "":
+            splited = self.authcode.split(":")
+            if time.time() - int(splited[-1]) < self.TIMEOUT:
+                raise ValidationError("5분 이후에 인증코드를 생성할 수 있습니다.")
+        
+        authcode = self._create_authcode()
+        return authcode
+
+    def check_authcode(self, authcode):
+        # 인증코드가 없을 때
+        if self.authcode == "":
+            raise ValidationError("먼저 인증코드를 생성해 주세요.")
+        # 만료시간 체크
+        splited = self.authcode.split(":")
+        if time.time() - int(splited[-1]) > self.TIMEOUT:
+            raise ValidationError("인증코드가 만료됐습니다. 인증코드를 새로 생성해 주세요.")
+        # 인증코드가 일치
+        if splited[0] == authcode:
+            return True
+        else:
+            return False
+
+    def change_lostpassword(self, password):
+        self.authcode = ""
+        self.set_password(password)
+        self.save()
+
+    # def check_password(self):
+    #     pass
+
+    def change_password(self, password, new_password):
+        # password -X-> db
+        # password -> 암호화 -> db
+        # if self.password == password:
+        if self.check_password(password):
+            self.set_password(new_password)
+            self.save()
+        else:
+            raise ValidationError("비밀번호 변경이 실패했습니다.")
+
+    def upload_profile(self, profile_image):
+        # image/jpg
+        if 'image' not in profile_image.content_type:
+            raise ValidationError("이미지 파일이 아닙니다. 이미지 파일을 업로드 해주세요.")
+        ext = profile_image.content_type.split("/")[-1]
+        self.profile.save(f"profiles/{self.pk}/profile-{int(time.time())}.{ext}", profile_image.file)
+        return self
+        
+    def delete_profile(self):
+        self.profile = None
+        self.save()
+        return self
--- service/users/urls.py
+++ service/users/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-from django.contrib import admin
-from django.urls import include
 
 from .views import (
     AuthViewSet,
@@ -11,7 +9,40 @@ signup = AuthViewSet.as_view({
     'post': 'signup'
 })
 
+signin = AuthViewSet.as_view({
+    'post': 'signin'
+})
+
+authcode = AuthViewSet.as_view({
+    'post': 'create_authcode',
+    'put': 'check_authcode',
+})
+
+change_lostpassword = AuthViewSet.as_view({
+    'put': 'change_lostpassword'
+})
+
+user_detail = UserViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'partial_update',
+    'patch': 'partial_update'
+})
+
+change_user_password = UserViewSet.as_view({
+    'put': 'change_password'
+})
+
+user_profile = UserViewSet.as_view({
+    'put': 'upload_profile',
+    'delete': 'delete_profile'
+})
+
 urlpatterns = [
     path('/signup', signup),
-    path('users', include('users.urls')),
+    path('/signin', signin),
+    path('/authcode', authcode),
+    path('/password', change_lostpassword),
+    path('/<int:pk>', user_detail),
+    path('/<int:pk>/password', change_user_password),
+    path('/<int:pk>/profile', user_profile),
 ]
