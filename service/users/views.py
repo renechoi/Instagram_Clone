92aa6048595e4d708955ff52ed8e592338422995
renechoi <renechoi90@gmail.com>
2022-12-09 06:23:18 +0900

    implement password authentication

--- service/users/views.py
+++ service/users/views.py
@@ -63,3 +63,135 @@ class AuthViewSet(ModelViewSet):
                 },
                 status=status.HTTP_404_NOT_FOUND
             )
+            
+    
+    def create_authcode(self, request):
+        email = request.data["email"]
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            return Response(
+                {
+                    "message": "해당 이메일이 존재하지 않습니다. 이메일을 확인해주세요."
+                },
+                status=status.HTTP_404_NOT_FOUND
+            )
+        try:
+            authcode = user.create_authcode()
+        except Exception as e:
+            return Response(
+                {
+                    "message": str(e)
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        return Response(
+            {
+                "authcode": authcode
+            },
+            status=status.HTTP_200_OK
+        )
+
+    def check_authcode(self, request):
+        # 이메일 확인하기
+        email = request.data.get('email', None)
+        if email is None:
+            return Response(
+                {
+                    "message": "이메일을 입력해주세요."
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        # 인증코드 확인하기
+        authcode = request.data.get('authcode', None)
+        if authcode is None:
+            return Response(
+                {
+                    "message": "인증코드를 입력해주세요."
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            
+            )
+
+        # 해당 이메일 사용자 객체 가져오기
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            return Response(
+                {
+                    "message": "해당 이메일을 가진 사용자가 존재하지 않습니다. 이메일을 확인해주세요."
+                },
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        # 인증코드 일치여부 확인
+        try:
+            result = user.check_authcode(authcode)
+        except Exception as e:
+            return Response(
+                {
+                    "message": str(e)
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        if result:
+            return Response(
+                status=status.HTTP_200_OK
+            )
+        else:
+            return Response(
+                {
+                    "message": "인증코드가 일치하지 않습니다."
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def change_lostpassword(self, request):
+        # 이메일 확인
+        email = request.data.get('email', None)
+        if email is None:
+            return Response(
+                {
+                    "message": "이메일이 없습니다."
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        # 비밀번호 확인
+        password = request.data.get('password', None)
+        if password is None:
+            return Response(
+                {
+                    "message": "비밀번호를 입력해주세요."
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        # 사용자 객체 가져오기
+        try:
+            user = User.objects.get(email=email)
+        except user.DoesNotExist:
+            return Response(
+                {
+                    "message": "해당 이메일을 가진 사용자가 존재하지 않습니다. 이메일을 확인해주세요."
+                },
+                status=status.HTTP_404_NOT_FOUND
+            )
+        # 비밀번호 변경
+        try:
+            user.change_lostpassword(password)
+        except Exception as e:
+            return Response(
+                {
+                    "message": str(e)
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        return Response(
+            {
+                "message": "비밀번호가 변경됐습니다."
+            },
+            status=status.HTTP_200_OK
+        )
